2ef6eaf21c874e54077d712e99d5871450ce4afc
kdann <k.dann2005@gmail.com>
2025-04-22 15:05:35 +0300

    Update

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,9 +3,10 @@
 import React, {useEffect, useState} from 'react';
 import {useCart} from "@/context/CoffeeContext";
 import s from "./page.module.scss"
-import { FaTrashAlt } from 'react-icons/fa';
+import {FaTrashAlt} from 'react-icons/fa';
 import {NativeSelect} from "@mantine/core";
 import Buttons from '@/UI/Buttons/Buttons';
+import classNames from "classnames";
 
 const Page = () => {
     const {cartItems, removeFromCart} = useCart()
@@ -15,50 +16,68 @@ const Page = () => {
         <main className={s.cart}>
             <div className="container">
                 <h1 className={s.cart__title}>Cart</h1>
-                {cartItems?.length === 0 ? (
-                    <p>Cart is empty</p>
-                ) : (
-                    <div className={s.cart__wrapper}>
-                        <ul className={s.cart__list}>
-                            {cartItems?.map(item => (
-                                <li className={s.cart__list_item} key={item.id}>
-                                    <div className={s.item__img}><img src={item.thumbnail.url} alt=""/></div>
-                                    <div className={s.item__lines}>
-                                        <div className={s.item__lines_upper}>
-                                            <span>{item.name}</span>
-                                            <FaTrashAlt onClick={() => removeFromCart(item.id)}/>
-                                        </div>
-                                        <span className={s.item__lines_middle}>{item.size}</span>
-                                        <div className={s.item__lines_under}>
-                                            <NativeSelect
+                <div className={s.cart__wrapper}>
+                    <ul className={s.cart__list}>
+                        <li className={s.cart__list_item}>
+                            <div className={s.right__side}>
+                                <div className={s.item__img}><img src="/logo.svg" alt="SVG"/></div>
+                                <div className={s.item__info}>
+                                    <span>Coffee</span>
+                                    <span>XL</span>
+                                    <NativeSelect
+                                        size={"xs"}
+                                        radius={"md"}
+                                        data={['1', '2', '3', '4', '5']}
+                                    />
+                                </div>
+                            </div>
+                            <div className={s.left__side}>
+                                <div className={s.delete__button}>
+                                    <FaTrashAlt/>
+                                </div>
+                                <span>19.99$</span>
+                            </div>
+                        </li>
+                        {/*{cartItems?.map(item => (*/}
+                        {/*    <li className={s.cart__list_item} key={item.id}>*/}
+                        {/*        <div className={s.item__img}><img src={item.thumbnail.url} alt=""/></div>*/}
+                        {/*        <div className={s.item__lines}>*/}
+                        {/*            <div className={s.item__lines_upper}>*/}
+                        {/*                <span>{item.name}</span>*/}
+                        {/*                <FaTrashAlt onClick={() => removeFromCart(item.id)}/>*/}
+                        {/*            </div>*/}
+                        {/*            <span className={s.item__lines_middle}>{item.size}</span>*/}
+                        {/*            <div className={s.item__lines_under}>*/}
+                        {/*                <NativeSelect*/}
 
-                                                size="xs"
-                                                radius="md"
-                                                data={['1', '2', '3', '4', '5']}
-                                            />
-                                            <span>{item.pricing.priceRange.start.gross.amount}</span>
-                                        </div>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                        <div>
-                            <div>
-                                <span>Price:</span>
-                                <span>$19.99</span>
+                        {/*                    size="xs"*/}
+                        {/*                    radius="md"*/}
+                        {/*                    data={['1', '2', '3', '4', '5']}*/}
+                        {/*                />*/}
+                        {/*                <span>{item.pricing.priceRange.start.gross.amount}</span>*/}
+                        {/*            </div>*/}
+                        {/*        </div>*/}
+                        {/*    </li>*/}
+                        {/*))}*/}
+                    </ul>
+                    <div className={s.total}>
+                        <div className={s.total__content}>
+                            <div className={classNames(s.block, s.price)}>
+                                <p>Price: </p>
+                                <span>19.99$</span>
                             </div>
-                            <div>
-                                <span>Delivery:</span>
-                                <span>$10.00</span>
+                            <div className={classNames(s.block, s.discount)}>
+                                <p>Discount: </p>
+                                <span>0%</span>
                             </div>
-                            <div>
-                                <span>Total price:</span>
-                                <span>$29.99</span>
+                            <div className={classNames(s.block, s["total-price"])}>
+                                <p>Total: </p>
+                                <span>19.99$</span>
                             </div>
-                            <Buttons text={"Checkout"} isBig={true} isTransparent={false}/>
+                            <Buttons text={"Checkout"} isBig={true}/>
                         </div>
                     </div>
-                )}
+                </div>
             </div>
         </main>
     );
